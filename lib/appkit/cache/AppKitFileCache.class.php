b748152d611113ed285f8b97b45cff6c0002e41c
Marius Hein <marius.hein@netways.de>
2009-10-03 09:59:45 +0200

    * prepared cache interface * simple file cache

---
+++ lib/appkit/cache/AppKitFileCache.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class AppKitFileCache extends AppKitCache {
+	
+	protected function storeData($region_name = null) {
+		
+	}	
+	
+	protected function restoreData($region_name = null) {
+		
+	}
+	
+}
+
+?>
