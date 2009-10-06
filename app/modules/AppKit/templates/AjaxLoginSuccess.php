5f506382a2ea82f41eebcbcce26efaefc5dcf1ec
Marius Hein <marius.hein@netways.de>
2009-10-06 16:29:53 +0200

    * Remove old plain icinga actions * Prepared ajax login action

--- app/modules/AppKit/templates/AjaxLoginSuccess.php
+++ app/modules/AppKit/templates/AjaxLoginSuccess.php
@@ -1,15 +1,50 @@
 <?php 
 	$htmlid = AppKitRandomUtil::genSimpleId(10);
 ?>
-<?php if ($us->isAuthenticated() !== true) { ?>
 <div id="<?php echo $htmlid; ?>"></div>
 <script type="text/javascript">
-
+(function() {
 Ext.onReady(function() {
 
-	oWin
+	var oWin = new Ext.Window({
+		title: '<?php echo $tm->_("Login"); ?>',
+		closable: true,
+		modal: true,
+		renderTo: Ext.getBody(),
+		width: 300,
+		layout: 'form',
+		defaultType: 'textfield',
+		bodyStyle: 'padding: 5px',
+		
+		items: [{
+			fieldLabel: '<?php echo $tm->_("User"); ?>',
+			name: 'user'
+		}, {
+			fieldLabel: '<?php echo $tm->_("Password"); ?>',
+			name: 'password',
+			inputType: 'password'
+		}],
+		
+		buttons: [
+			{
+				text: '<?php echo $tm->_("Try"); ?>',
+				handler: function(o, e) {
+					Ext.Msg.alert("Test", "Try login ... ");
+				}
+			}, {
+				text: '<?php echo $tm->_("Abort"); ?>',
+				handler: function(o, e) {
+					oWin.hide();
+				}
+			}
+		]
+		
+	});
+	
+	<?php if ($us->isAuthenticated() !== true) { ?>
+	oWin.show();
+	<?php } ?>
 
 });
-
+})();
 </script>
-<?php } ?>
