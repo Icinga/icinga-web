4fe4079931355a2aa52b1228e5edfb5090938e17
Christian Doebler <christian.doebler@netways.de>
2009-10-23 14:51:30 +0200

    * modified welcome message

--- app/modules/Cronks/templates/System/PortalHelloSuccess.php
+++ app/modules/Cronks/templates/System/PortalHelloSuccess.php
@@ -1,13 +1,8 @@
-<h1>Welcome to the Icinga cronk portal</h1>
-
+<br/>
+<h1>Welcome to the Icinga cronk portal!</h1><br/>
 <p>
-	Cronks are simple widgets to add content to your view. Every view is a portlet container or
-	a single view of cronk.
-	
-	Well, don not ask why this is called cronk!
-</p>
-
-<p>
-<strong>random-<?php echo AppKitRandomUtil::genSimpleId('15'); ?></strong><br />
-Parent component: <i><?php echo $rd->getParameter('parentid'); ?></i>
+This is our alpha release. Feel free to poke around and don't forget to visit the project homepage to post bug advisories or feature requests.<br/><br/>
+What are Cronks? Simply put, they are widgets for the Icinga web front end - with a cooler name.<br/><br/>
+Have fun<br/><br/>
+Team Icinga
 </p>
