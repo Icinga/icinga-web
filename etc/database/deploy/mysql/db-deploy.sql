08177d51b4941491539a8129569e3e2311dbe563
Marius Hein <marius.hein@netways.de>
2009-12-03 16:26:18 +0100

    * Database fixes * Prepared multiple scheme support (oracle, pgsql) * Added principals for later privileges

--- etc/database/deploy/mysql/db-deploy.sql
+++ etc/database/deploy/mysql/db-deploy.sql
@@ -88,3 +88,96 @@ INSERT INTO `nsm_user_role` VALUES (1,4),(1,5);
 
 UPDATE changelog SET complete_dt = NOW() WHERE change_number = 2 AND delta_set = 'Main';
 -- Fragment ends: 2 --
+-- Fragment begins: 3 --
+INSERT INTO changelog (change_number, delta_set, start_dt, applied_by, description) VALUES (3, 'Main', NOW(), 'dbdeploy', '3-principal-scheme.sql');
+-- //
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+CREATE TABLE `nsm_principal_target` (
+  `pt_id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `pt_principal_id` INT(11) NOT NULL ,
+  `pt_target_id` INT(11) NOT NULL ,
+  INDEX `fk_nsm_principal_has_nsm_target_nsm_principal1` (`pt_principal_id` ASC) ,
+  INDEX `fk_nsm_principal_has_nsm_target_nsm_target1` (`pt_target_id` ASC) ,
+  PRIMARY KEY (`pt_id`) ,
+  CONSTRAINT `fk_nsm_principal_has_nsm_target_nsm_principal1`
+    FOREIGN KEY (`pt_principal_id` )
+    REFERENCES `nsm_principal` (`principal_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_nsm_principal_has_nsm_target_nsm_target1`
+    FOREIGN KEY (`pt_target_id` )
+    REFERENCES `nsm_target` (`target_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE `nsm_principal` (
+  `principal_id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `principal_user_id` INT(10) NULL DEFAULT NULL ,
+  `principal_role_id` INT(10) NULL DEFAULT NULL ,
+  `principal_type` ENUM('role', 'user') NOT NULL ,
+  `principal_disabled` TINYINT(4) NULL DEFAULT 0 ,
+  PRIMARY KEY (`principal_id`) ,
+  INDEX `fk_nsm_principle_nsm_user1` (`principal_user_id` ASC) ,
+  INDEX `fk_nsm_principle_nsm_role1` (`principal_role_id` ASC) ,
+  CONSTRAINT `fk_nsm_principle_nsm_user1`
+    FOREIGN KEY (`principal_user_id` )
+    REFERENCES `nsm_user` (`user_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_nsm_principle_nsm_role1`
+    FOREIGN KEY (`principal_role_id` )
+    REFERENCES `nsm_role` (`role_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE `nsm_target_value` (
+  `tv_pt_id` INT(11) NOT NULL ,
+  `tv_key` VARCHAR(45) NOT NULL ,
+  `tv_val` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`tv_pt_id`, `tv_key`) ,
+  INDEX `fk_nsm_target_value_nsm_principal_target1` (`tv_pt_id` ASC) ,
+  CONSTRAINT `fk_nsm_target_value_nsm_principal_target1`
+    FOREIGN KEY (`tv_pt_id` )
+    REFERENCES `nsm_principal_target` (`pt_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE TABLE `nsm_target` (
+  `target_id` INT(11) NOT NULL ,
+  `target_name` VARCHAR(45) NULL DEFAULT NULL ,
+  `target_description` VARCHAR(100) NULL DEFAULT NULL ,
+  `target_class` VARCHAR(45) NULL DEFAULT NULL ,
+  PRIMARY KEY (`target_id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+UPDATE changelog SET complete_dt = NOW() WHERE change_number = 3 AND delta_set = 'Main';
+-- Fragment ends: 3 --
+-- Fragment begins: 4 --
+INSERT INTO changelog (change_number, delta_set, start_dt, applied_by, description) VALUES (4, 'Main', NOW(), 'dbdeploy', '4-principal-data.sql');
+-- //
+
+INSERT INTO `nsm_principal` VALUES 
+	(1,1,NULL,'user',0),
+	(2,NULL,3,'role',0),
+	(3,NULL,4,'role',0),
+	(4,NULL,5,'role',0);
+
+UPDATE changelog SET complete_dt = NOW() WHERE change_number = 4 AND delta_set = 'Main';
+-- Fragment ends: 4 --
--- etc/database/deploy/mysql/db-undo.sql
+++ etc/database/deploy/mysql/db-undo.sql
@@ -1,5 +1,13 @@
 
 
+-- //DELETE FROM changelog WHERE change_number = 4 AND delta_set = 'Main';
+-- Fragment ends: 4 --
+
+
+-- //DELETE FROM changelog WHERE change_number = 3 AND delta_set = 'Main';
+-- Fragment ends: 3 --
+
+
 	TRUNCATE TABLE nsm_user_role;
 	TRUNCATE TABLE nsm_role;
 	TRUNCATE TABLE nsm_user;
