f1887ae340486b575174bc4aac09078ceddf687c
Marius Hein <marius.hein@netways.de>
2010-05-18 12:01:24 +0200

    * Cleanup database changes

---
+++ etc/database/rescue/mysql/20100518115927-rescue-data.sql
@@ -0,0 +1,134 @@
+-- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: icinga_web
+-- ------------------------------------------------------
+-- Server version	5.5.3-m3-log
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,MYSQL40' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `migration_version`
+--
+
+LOCK TABLES `migration_version` WRITE;
+/*!40000 ALTER TABLE `migration_version` DISABLE KEYS */;
+INSERT INTO `migration_version` VALUES (11);
+/*!40000 ALTER TABLE `migration_version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_log`
+--
+
+LOCK TABLES `nsm_log` WRITE;
+/*!40000 ALTER TABLE `nsm_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nsm_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_principal`
+--
+
+LOCK TABLES `nsm_principal` WRITE;
+/*!40000 ALTER TABLE `nsm_principal` DISABLE KEYS */;
+INSERT INTO `nsm_principal` VALUES (1,1,NULL,'user',0),(2,NULL,2,'role',0),(3,NULL,3,'role',0),(4,NULL,1,'role',0),(5,NULL,4,'role',0);
+/*!40000 ALTER TABLE `nsm_principal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_principal_target`
+--
+
+LOCK TABLES `nsm_principal_target` WRITE;
+/*!40000 ALTER TABLE `nsm_principal_target` DISABLE KEYS */;
+INSERT INTO `nsm_principal_target` VALUES (1,2,8),(2,3,9),(3,3,10),(4,3,11),(5,4,8),(6,5,7);
+/*!40000 ALTER TABLE `nsm_principal_target` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_role`
+--
+
+LOCK TABLES `nsm_role` WRITE;
+/*!40000 ALTER TABLE `nsm_role` DISABLE KEYS */;
+INSERT INTO `nsm_role` VALUES (1,'icinga_user','The default representation of a ICINGA user',0,'2009-04-20 15:56:52','2010-05-11 13:09:20',NULL),(2,'appkit_user','Appkit user test',0,'2009-02-19 09:17:37','2010-05-11 13:09:13',1),(3,'appkit_admin','AppKit admin',0,'2009-02-17 10:17:10','2010-05-11 13:08:55',2),(4,'guest','Unauthorized Guest',0,'2009-02-17 10:17:10','2010-05-11 13:09:26',NULL);
+/*!40000 ALTER TABLE `nsm_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_session`
+--
+
+LOCK TABLES `nsm_session` WRITE;
+/*!40000 ALTER TABLE `nsm_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nsm_session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_target`
+--
+
+LOCK TABLES `nsm_target` WRITE;
+/*!40000 ALTER TABLE `nsm_target` DISABLE KEYS */;
+INSERT INTO `nsm_target` VALUES (1,'IcingaHostgroup','Limit data access to specific hostgroups','IcingaDataHostgroupPrincipalTarget','icinga'),(2,'IcingaServicegroup','Limit data access to specific servicegroups','IcingaDataServicegroupPrincipalTarget','icinga'),(3,'IcingaHostCustomVariablePair','Limit data access to specific custom variables','IcingaDataHostCustomVariablePrincipalTarget','icinga'),(4,'IcingaServiceCustomVariablePair','Limit data access to specific custom variables','IcingaDataServiceCustomVariablePrincipalTarget','icinga'),(5,'IcingaContactgroup','Limit data access to users contact group membership','IcingaDataContactgroupPrincipalTarget','icinga'),(6,'IcingaCommandRo','Limit access to commands','IcingaDataCommandRoPrincipalTarget','icinga'),(7,'appkit.access','Access to login-page (which, actually, means no access)','','credential'),(8,'icinga.user','Access to icinga','','credential'),(9,'appkit.admin.groups','Access to group editor','','credential'),(10,'appkit.admin.users','Access to user editor','','credential'),(11,'appkit.admin','Access to admin panel ','','credential'),(12,'appkit.user.dummy','Basic right for users','','credential'),(13,'module.heatmap','Allow access to the heatmap module','','credential'),(14,'module.heatmap.room.create','Allow creation of heatmaps','','credential'),(15,'module.heatmap.schedules','Allow management of schedules','','credential');
+/*!40000 ALTER TABLE `nsm_target` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_target_value`
+--
+
+LOCK TABLES `nsm_target_value` WRITE;
+/*!40000 ALTER TABLE `nsm_target_value` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nsm_target_value` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_user`
+--
+
+LOCK TABLES `nsm_user` WRITE;
+/*!40000 ALTER TABLE `nsm_user` DISABLE KEYS */;
+INSERT INTO `nsm_user` VALUES (1,0,'root','Root','Enoch','18242a1cc143f34faf45e0f08a962bd892ce58fb3aa8af3392da9149838c73d9','5136137213e7403acdbc564a3203c721cea1ea9d4c28b3722da104955861976f','root@localhost.local',0,'2009-02-18 10:12:59','2010-05-11 13:10:16');
+/*!40000 ALTER TABLE `nsm_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_user_preference`
+--
+
+LOCK TABLES `nsm_user_preference` WRITE;
+/*!40000 ALTER TABLE `nsm_user_preference` DISABLE KEYS */;
+INSERT INTO `nsm_user_preference` VALUES (1,1,NULL,'[]','de.icinga.ext.appstate','2010-05-11 13:07:58','2010-05-11 14:04:27');
+/*!40000 ALTER TABLE `nsm_user_preference` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `nsm_user_role`
+--
+
+LOCK TABLES `nsm_user_role` WRITE;
+/*!40000 ALTER TABLE `nsm_user_role` DISABLE KEYS */;
+INSERT INTO `nsm_user_role` VALUES (1,1),(1,2),(1,3);
+/*!40000 ALTER TABLE `nsm_user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'icinga_web'
+--
+
+--
+-- Dumping routines for database 'icinga_web'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-05-18 11:59:27
--- etc/database/rescue/mysql/20100518115927-rescue-schema.sql
+++ etc/database/rescue/mysql/20100518115927-rescue-schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.11
+-- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: icinga_web
 -- ------------------------------------------------------
--- Server version	5.1.40
+-- Server version	5.5.3-m3-log
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -11,18 +11,12 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `changelog`
+-- Table structure for table `migration_version`
 --
 
-DROP TABLE IF EXISTS `changelog`;
-CREATE TABLE `changelog` (
-  `change_number` bigint(20) NOT NULL,
-  `delta_set` varchar(10) NOT NULL,
-  `start_dt` timestamp NOT NULL,
-  `complete_dt` timestamp NULL DEFAULT NULL,
-  `applied_by` varchar(100) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  PRIMARY KEY (`change_number`,`delta_set`)
+DROP TABLE IF EXISTS `migration_version`;
+CREATE TABLE `migration_version` (
+  `version` int(11) DEFAULT NULL
 ) TYPE=InnoDB;
 
 --
@@ -31,13 +25,13 @@ CREATE TABLE `changelog` (
 
 DROP TABLE IF EXISTS `nsm_log`;
 CREATE TABLE `nsm_log` (
-  `log_id` int(10) NOT NULL AUTO_INCREMENT,
-  `log_level` int(10) NOT NULL,
-  `log_message` varchar(4000) NOT NULL,
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `log_level` int(11) NOT NULL,
+  `log_message` text NOT NULL,
   `log_created` datetime NOT NULL,
   `log_modified` datetime NOT NULL,
   PRIMARY KEY (`log_id`)
-) TYPE=InnoDB AUTO_INCREMENT=2;
+) TYPE=InnoDB;
 
 --
 -- Table structure for table `nsm_principal`
@@ -46,16 +40,16 @@ CREATE TABLE `nsm_log` (
 DROP TABLE IF EXISTS `nsm_principal`;
 CREATE TABLE `nsm_principal` (
   `principal_id` int(11) NOT NULL AUTO_INCREMENT,
-  `principal_user_id` int(10) DEFAULT NULL,
-  `principal_role_id` int(10) DEFAULT NULL,
-  `principal_type` enum('role','user') NOT NULL,
+  `principal_user_id` int(11) DEFAULT NULL,
+  `principal_role_id` int(11) DEFAULT NULL,
+  `principal_type` varchar(4) NOT NULL,
   `principal_disabled` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`principal_id`),
-  KEY `fk_nsm_principle_nsm_user1` (`principal_user_id`),
-  KEY `fk_nsm_principle_nsm_role1` (`principal_role_id`),
-  CONSTRAINT `fk_nsm_principle_nsm_role1` FOREIGN KEY (`principal_role_id`) REFERENCES `nsm_role` (`role_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_nsm_principle_nsm_user1` FOREIGN KEY (`principal_user_id`) REFERENCES `nsm_user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) TYPE=InnoDB AUTO_INCREMENT=5;
+  KEY `nsm_principal_principal_role_id_nsm_role_role_id` (`principal_role_id`),
+  KEY `nsm_principal_principal_user_id_nsm_user_user_id` (`principal_user_id`),
+  CONSTRAINT `nsm_principal_principal_user_id_nsm_user_user_id` FOREIGN KEY (`principal_user_id`) REFERENCES `nsm_user` (`user_id`),
+  CONSTRAINT `nsm_principal_principal_role_id_nsm_role_role_id` FOREIGN KEY (`principal_role_id`) REFERENCES `nsm_role` (`role_id`)
+) TYPE=InnoDB AUTO_INCREMENT=6;
 
 --
 -- Table structure for table `nsm_principal_target`
@@ -67,11 +61,11 @@ CREATE TABLE `nsm_principal_target` (
   `pt_principal_id` int(11) NOT NULL,
   `pt_target_id` int(11) NOT NULL,
   PRIMARY KEY (`pt_id`),
-  KEY `fk_nsm_principal_has_nsm_target_nsm_principal1` (`pt_principal_id`),
-  KEY `fk_nsm_principal_has_nsm_target_nsm_target1` (`pt_target_id`),
-  CONSTRAINT `fk_nsm_principal_has_nsm_target_nsm_principal1` FOREIGN KEY (`pt_principal_id`) REFERENCES `nsm_principal` (`principal_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_nsm_principal_has_nsm_target_nsm_target1` FOREIGN KEY (`pt_target_id`) REFERENCES `nsm_target` (`target_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) TYPE=InnoDB;
+  KEY `nsm_principal_target_pt_principal_id_nsm_principal_principal_id` (`pt_principal_id`),
+  KEY `nsm_principal_target_pt_target_id_nsm_target_target_id` (`pt_target_id`),
+  CONSTRAINT `nsm_principal_target_pt_target_id_nsm_target_target_id` FOREIGN KEY (`pt_target_id`) REFERENCES `nsm_target` (`target_id`),
+  CONSTRAINT `nsm_principal_target_pt_principal_id_nsm_principal_principal_id` FOREIGN KEY (`pt_principal_id`) REFERENCES `nsm_principal` (`principal_id`)
+) TYPE=InnoDB AUTO_INCREMENT=7;
 
 --
 -- Table structure for table `nsm_role`
@@ -79,14 +73,17 @@ CREATE TABLE `nsm_principal_target` (
 
 DROP TABLE IF EXISTS `nsm_role`;
 CREATE TABLE `nsm_role` (
-  `role_id` int(10) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
   `role_name` varchar(40) NOT NULL,
   `role_description` varchar(255) DEFAULT NULL,
-  `role_disabled` tinyint(1) NOT NULL DEFAULT '0',
+  `role_disabled` tinyint(4) NOT NULL DEFAULT '0',
   `role_created` datetime NOT NULL,
   `role_modified` datetime NOT NULL,
-  PRIMARY KEY (`role_id`)
-) TYPE=InnoDB AUTO_INCREMENT=6;
+  `role_parent` int(11) DEFAULT NULL,
+  PRIMARY KEY (`role_id`),
+  KEY `nsm_role_role_parent_nsm_role_role_id` (`role_parent`),
+  CONSTRAINT `nsm_role_role_parent_nsm_role_role_id` FOREIGN KEY (`role_parent`) REFERENCES `nsm_role` (`role_id`)
+) TYPE=InnoDB AUTO_INCREMENT=5;
 
 --
 -- Table structure for table `nsm_session`
@@ -94,7 +91,7 @@ CREATE TABLE `nsm_role` (
 
 DROP TABLE IF EXISTS `nsm_session`;
 CREATE TABLE `nsm_session` (
-  `session_entry_id` int(10) NOT NULL AUTO_INCREMENT,
+  `session_entry_id` int(11) NOT NULL AUTO_INCREMENT,
   `session_id` varchar(255) NOT NULL,
   `session_name` varchar(255) NOT NULL,
   `session_data` longblob NOT NULL,
@@ -102,7 +99,7 @@ CREATE TABLE `nsm_session` (
   `session_created` datetime NOT NULL,
   `session_modified` datetime NOT NULL,
   PRIMARY KEY (`session_entry_id`)
-) TYPE=InnoDB AUTO_INCREMENT=132;
+) TYPE=InnoDB;
 
 --
 -- Table structure for table `nsm_target`
@@ -110,12 +107,13 @@ CREATE TABLE `nsm_session` (
 
 DROP TABLE IF EXISTS `nsm_target`;
 CREATE TABLE `nsm_target` (
-  `target_id` int(11) NOT NULL,
-  `target_name` varchar(45) DEFAULT NULL,
+  `target_id` int(11) NOT NULL AUTO_INCREMENT,
+  `target_name` varchar(45) NOT NULL,
   `target_description` varchar(100) DEFAULT NULL,
-  `target_class` varchar(80) DEFAULT NULL,
+  `target_class` varchar(80) NOT NULL,
+  `target_type` varchar(45) NOT NULL,
   PRIMARY KEY (`target_id`)
-) TYPE=InnoDB;
+) TYPE=InnoDB AUTO_INCREMENT=16;
 
 --
 -- Table structure for table `nsm_target_value`
@@ -123,12 +121,11 @@ CREATE TABLE `nsm_target` (
 
 DROP TABLE IF EXISTS `nsm_target_value`;
 CREATE TABLE `nsm_target_value` (
-  `tv_pt_id` int(11) NOT NULL,
-  `tv_key` varchar(45) NOT NULL,
+  `tv_pt_id` int(11) NOT NULL DEFAULT '0',
+  `tv_key` varchar(45) NOT NULL DEFAULT '',
   `tv_val` varchar(45) NOT NULL,
   PRIMARY KEY (`tv_pt_id`,`tv_key`),
-  KEY `fk_nsm_target_value_nsm_principal_target1` (`tv_pt_id`),
-  CONSTRAINT `fk_nsm_target_value_nsm_principal_target1` FOREIGN KEY (`tv_pt_id`) REFERENCES `nsm_principal_target` (`pt_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `nsm_target_value_tv_pt_id_nsm_principal_target_pt_id` FOREIGN KEY (`tv_pt_id`) REFERENCES `nsm_principal_target` (`pt_id`)
 ) TYPE=InnoDB;
 
 --
@@ -137,19 +134,18 @@ CREATE TABLE `nsm_target_value` (
 
 DROP TABLE IF EXISTS `nsm_user`;
 CREATE TABLE `nsm_user` (
-  `user_id` int(10) NOT NULL AUTO_INCREMENT,
-  `user_account` int(40) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_account` int(11) NOT NULL,
   `user_name` varchar(18) NOT NULL,
   `user_lastname` varchar(40) NOT NULL,
   `user_firstname` varchar(40) NOT NULL,
   `user_password` varchar(64) NOT NULL,
   `user_salt` varchar(64) NOT NULL,
   `user_email` varchar(40) NOT NULL,
-  `user_disabled` tinyint(1) NOT NULL DEFAULT '1',
+  `user_disabled` tinyint(4) NOT NULL DEFAULT '1',
   `user_created` datetime NOT NULL,
   `user_modified` datetime NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `nsm_user_name_idx` (`user_name`)
+  PRIMARY KEY (`user_id`)
 ) TYPE=InnoDB AUTO_INCREMENT=2;
 
 --
@@ -158,17 +154,17 @@ CREATE TABLE `nsm_user` (
 
 DROP TABLE IF EXISTS `nsm_user_preference`;
 CREATE TABLE `nsm_user_preference` (
-  `upref_id` int(10) NOT NULL AUTO_INCREMENT,
-  `upref_user_id` int(10) NOT NULL,
+  `upref_id` int(11) NOT NULL AUTO_INCREMENT,
+  `upref_user_id` int(11) NOT NULL,
   `upref_val` varchar(100) DEFAULT NULL,
-  `upref_longval` blob,
+  `upref_longval` longblob,
   `upref_key` varchar(50) NOT NULL,
   `upref_created` datetime NOT NULL,
   `upref_modified` datetime NOT NULL,
   PRIMARY KEY (`upref_id`),
-  UNIQUE KEY `nsm_user_preference_userkey` (`upref_user_id`,`upref_key`),
-  CONSTRAINT `nsm_user_nsm_user_preference_fk` FOREIGN KEY (`upref_user_id`) REFERENCES `nsm_user` (`user_id`)
-) TYPE=InnoDB;
+  KEY `nsm_user_preference_upref_user_id_nsm_user_user_id` (`upref_user_id`),
+  CONSTRAINT `nsm_user_preference_upref_user_id_nsm_user_user_id` FOREIGN KEY (`upref_user_id`) REFERENCES `nsm_user` (`user_id`)
+) TYPE=InnoDB AUTO_INCREMENT=2;
 
 --
 -- Table structure for table `nsm_user_role`
@@ -176,13 +172,21 @@ CREATE TABLE `nsm_user_preference` (
 
 DROP TABLE IF EXISTS `nsm_user_role`;
 CREATE TABLE `nsm_user_role` (
-  `usro_user_id` int(10) NOT NULL,
-  `usro_role_id` int(10) NOT NULL,
+  `usro_user_id` int(11) NOT NULL DEFAULT '0',
+  `usro_role_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`usro_user_id`,`usro_role_id`),
-  KEY `nsm_usro_role_fk` (`usro_role_id`),
-  CONSTRAINT `nsm_usro_role_fk` FOREIGN KEY (`usro_role_id`) REFERENCES `nsm_role` (`role_id`),
-  CONSTRAINT `nsm_usro_user_fk` FOREIGN KEY (`usro_user_id`) REFERENCES `nsm_user` (`user_id`)
+  KEY `nsm_user_role_usro_role_id_nsm_role_role_id` (`usro_role_id`),
+  CONSTRAINT `nsm_user_role_usro_user_id_nsm_user_user_id` FOREIGN KEY (`usro_user_id`) REFERENCES `nsm_user` (`user_id`),
+  CONSTRAINT `nsm_user_role_usro_role_id_nsm_role_role_id` FOREIGN KEY (`usro_role_id`) REFERENCES `nsm_role` (`role_id`)
 ) TYPE=InnoDB;
+
+--
+-- Dumping events for database 'icinga_web'
+--
+
+--
+-- Dumping routines for database 'icinga_web'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -190,4 +194,4 @@ CREATE TABLE `nsm_user_role` (
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-12-04 12:11:35
+-- Dump completed on 2010-05-18 11:59:27
