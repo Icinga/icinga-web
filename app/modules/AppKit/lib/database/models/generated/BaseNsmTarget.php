3d6f64a2b5ab7069b7b22e787f90a6ac4580c059
jmosshammer <jmosshammer@ws-jmosshammer.(none)>
2010-06-16 11:02:49 +0200

    * Re-added Ids of initial data, although Postgre won't auto update its seqeunce

--- app/modules/AppKit/lib/database/models/generated/BaseNsmTarget.php
+++ app/modules/AppKit/lib/database/models/generated/BaseNsmTarget.php
@@ -79,19 +79,19 @@ abstract class BaseNsmTarget extends Doctrine_Record
     
     public static function getInitialData() {
 		return array(
-			array(/*'target_id'=>1,*/'target_name'=>"IcingaHostgroup",'target_description'=>"Limit data access to specific hostgroups",'target_class'=>"IcingaDataHostgroupPrincipalTarget",'target_type'=>"icinga"),
-			array(/*'target_id'=>2,*/'target_name'=>"IcingaServicegroup",'target_description'=>"Limit data access to specific servicegroups",'target_class'=>"IcingaDataServicegroupPrincipalTarget",'target_type'=>"icinga"),
-			array(/*'target_id'=>3,*/'target_name'=>"IcingaHostCustomVariablePair",'target_description'=>"Limit data access to specific custom variables",'target_class'=>"IcingaDataHostCustomVariablePrincipalTarget",'target_type'=>"icinga"),
-			array(/*'target_id'=>4,*/'target_name'=>"IcingaServiceCustomVariablePair",'target_description'=>"Limit data access to specific custom variables",'target_class'=>"IcingaDataServiceCustomVariablePrincipalTarget",'target_type'=>"icinga"),
-			array(/*'target_id'=>5,*/'target_name'=>"IcingaContactgroup",'target_description'=>"Limit data access to users contact group membership",'target_class'=>"IcingaDataContactgroupPrincipalTarget",'target_type'=>"icinga"),
-			array(/*'target_id'=>6,*/'target_name'=>"IcingaCommandRo",'target_description'=>"Limit access to commands",'target_class'=>"IcingaDataCommandRoPrincipalTarget",'target_type'=>"icinga"),
-			array(/*'target_id'=>7,*/'target_name'=>"appkit.access",'target_description'=>"Access to login-page (which, actually, means no access)",'target_class'=>"",'target_type'=>"credential"),
-			array(/*'target_id'=>8,*/'target_name'=>"icinga.user",'target_description'=>"Access to icinga",'target_class'=>"",'target_type'=>"credential"),
-			array(/*'target_id'=>9,*/'target_name'=>"appkit.admin.groups",'target_description'=>"Access to group editor",'target_class'=>"",'target_type'=>"credential"),
-			array(/*'target_id'=>10,*/'target_name'=>"appkit.admin.users",'target_description'=>"Access to user editor",'target_class'=>"",'target_type'=>"credential"),
-			array(/*'target_id'=>11,*/'target_name'=>"appkit.admin",'target_description'=>"Access to admin panel ",'target_class'=>"",'target_type'=>"credential"),
-			array(/*'target_id'=>12,*/'target_name'=>"appkit.user.dummy",'target_description'=>"Basic right for users",'target_class'=>"",'target_type'=>"credential"),
-			array(/*'target_id'=>13,*/'target_name'=>"appkit.api.access",'target_description'=>"Access to web-based api adapter",'target_class'=>"",'target_type'=>"credential")
+			array('target_id'=>1,'target_name'=>"IcingaHostgroup",'target_description'=>"Limit data access to specific hostgroups",'target_class'=>"IcingaDataHostgroupPrincipalTarget",'target_type'=>"icinga"),
+			array('target_id'=>2,'target_name'=>"IcingaServicegroup",'target_description'=>"Limit data access to specific servicegroups",'target_class'=>"IcingaDataServicegroupPrincipalTarget",'target_type'=>"icinga"),
+			array('target_id'=>3,'target_name'=>"IcingaHostCustomVariablePair",'target_description'=>"Limit data access to specific custom variables",'target_class'=>"IcingaDataHostCustomVariablePrincipalTarget",'target_type'=>"icinga"),
+			array('target_id'=>4,'target_name'=>"IcingaServiceCustomVariablePair",'target_description'=>"Limit data access to specific custom variables",'target_class'=>"IcingaDataServiceCustomVariablePrincipalTarget",'target_type'=>"icinga"),
+			array('target_id'=>5,'target_name'=>"IcingaContactgroup",'target_description'=>"Limit data access to users contact group membership",'target_class'=>"IcingaDataContactgroupPrincipalTarget",'target_type'=>"icinga"),
+			array('target_id'=>6,'target_name'=>"IcingaCommandRo",'target_description'=>"Limit access to commands",'target_class'=>"IcingaDataCommandRoPrincipalTarget",'target_type'=>"icinga"),
+			array('target_id'=>7,'target_name'=>"appkit.access",'target_description'=>"Access to login-page (which, actually, means no access)",'target_class'=>"",'target_type'=>"credential"),
+			array('target_id'=>8,'target_name'=>"icinga.user",'target_description'=>"Access to icinga",'target_class'=>"",'target_type'=>"credential"),
+			array('target_id'=>9,'target_name'=>"appkit.admin.groups",'target_description'=>"Access to group editor",'target_class'=>"",'target_type'=>"credential"),
+			array('target_id'=>10,'target_name'=>"appkit.admin.users",'target_description'=>"Access to user editor",'target_class'=>"",'target_type'=>"credential"),
+			array('target_id'=>11,'target_name'=>"appkit.admin",'target_description'=>"Access to admin panel ",'target_class'=>"",'target_type'=>"credential"),
+			array('target_id'=>12,'target_name'=>"appkit.user.dummy",'target_description'=>"Basic right for users",'target_class'=>"",'target_type'=>"credential"),
+			array('target_id'=>13,'target_name'=>"appkit.api.access",'target_description'=>"Access to web-based api adapter",'target_class'=>"",'target_type'=>"credential")
 		);
     }
 }
