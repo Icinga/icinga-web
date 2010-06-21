451261825904ddd5ceeec430b6451a8826c746fb
Marius Hein <mhein@ws-jmosshammer.(none)>
2010-06-21 18:15:49 +0200

    * New status throbber

--- app/modules/Cronks/templates/System/StatusOverallSuccess.php
+++ app/modules/Cronks/templates/System/StatusOverallSuccess.php
@@ -1,6 +1,14 @@
 <script type="text/javascript">
 Cronk.util.initEnvironment("<?php echo $rd->getParameter('parentid'); ?>", function() {
 
+	var statusOverallRenderer = {
+		prepareData: function(data, recordIndex, record) {
+			data.state_org = data.state;
+			data.state = Icinga.StatusData.wrapElement(data.type, data.state, data.count + ' {0}' );
+			return data;
+		}
+	};
+
 	var CE = this;
 
 	var ds = new Ext.data.JsonStore({
@@ -10,6 +18,13 @@ Cronk.util.initEnvironment("<?php echo $rd->getParameter('parentid'); ?>", funct
 
 	ds.load();
 
+	var statusOverallRefreshTask = {
+		run: function() { ds.reload(); },
+		interval: (1000*180)
+	}
+
+	AppKit.getTr().start(statusOverallRefreshTask);
+
 	var p = new Ext.Panel({
 		layout: 'fit',
 		width: 400,
@@ -18,15 +33,32 @@ Cronk.util.initEnvironment("<?php echo $rd->getParameter('parentid'); ?>", funct
 			xtype: 'dataview',
 			store: ds,
 			autoHeight: true,
+			prepareData: statusOverallRenderer.prepareData,
+			itemSelector:'div.icinga-overall-status-item',
+
+			listeners: {
+				click: function(dview, index, node, e) {
+					var d = dview.getStore().getAt(index).data;
+					var template = 'icinga-' + d.type + '-template';
+					var id = 'status-overall-' + d.type + d.state_org;
+					var c = {
+						
+					}
+				}
+			},
+
 			tpl: new Ext.XTemplate(
 				'<tpl for=".">',
+					'<tpl if="id==1">',
+					'<div class="icinga-overall-status-icon silk-database" title="' + _('Hosts') + '"></div>',
+					'</tpl>',
 					'<tpl if="id==5">',
-					'<div class="x-clear"></div>',
+					'<div class="x-clear icinga-overall-status-spacer"></div>',
+					'<div class="icinga-overall-status-icon silk-cog" title="' + _('Services') + '"></div>',
 					'</tpl>',
 					'<div class="icinga-overall-status-item" id="overall-status-{id}">',
-					'<span>({id})</span>',
-					'<span>{state}: </span>',
-					'<span>{count}</span></div>',
+					'<span>{state}</span>',
+					'</div>',
 				'</tpl>',
 				'<div class="x-clear"></div>'
 			)
