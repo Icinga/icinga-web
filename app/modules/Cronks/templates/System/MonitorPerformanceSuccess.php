7a857adbd2cb350d2dafa85b7f53eeb6f4aedde8
Marius Hein <marius.hein@netways.de>
2010-09-23 16:50:44 +0200

    * Monitoring performance cronk (ref #796)

---
+++ app/modules/Cronks/templates/System/MonitorPerformanceSuccess.php
@@ -0,0 +1,75 @@
+<script type="text/javascript">
+Cronk.util.initEnvironment("<?php echo $rd->getParameter('parentid'); ?>", function() {
+	var CE = this;
+	
+	var ds = new Ext.data.JsonStore({
+		url: '<?php echo $ro->gen('cronks.monitorPerformance.json') ?>',
+		storeId: 'overall-status-store'
+	});
+
+	ds.load();
+	
+	var interval = <?php echo $us->getPrefVal('org.icinga.grid.refreshTime', AgaviConfig::get('modules.cronks.grid.refreshTime', 120)); ?>;
+	
+	var monitorPerformanceRefreshTask = {
+		run: function() { ds.reload(); },
+		interval: (1000*interval)
+	}
+
+	AppKit.getTr().start(monitorPerformanceRefreshTask);
+	
+	var monitorPerformancePanel = new Ext.DataView({
+		store: ds,
+		tpl: new Ext.XTemplate(
+			'<tpl for=".">',
+			
+			'<div class="float-container clearfix icinga-monitor-performance">',
+			
+			'<div class="icinga-monitor-performance-container-50">',
+			
+				'<div class="clearfix icinga-monitor-performance-container">',
+					'<div title="' + _('Hosts') + '" class="key icinga-icon-host"></div>',
+					'<div class="value">{NUM_ACTIVE_HOST_CHECKS} / {NUM_PASSIVE_HOST_CHECKS}</div>',
+				'</div>',
+			
+				'<div class="clearfix icinga-monitor-performance-container">',
+					'<div title="' + _('Host execution time') + '" class="key icinga-icon-execution-time"></div>',
+					'<div class="value">{HOST_EXECUTION_TIME_MIN} / {HOST_EXECUTION_TIME_AVG} / {HOST_EXECUTION_TIME_MAX}</div>',
+				'</div>',
+				
+				'<div class="clearfix icinga-monitor-performance-container">',
+					'<div title="' + _('Host latency') + '" class="key icinga-icon-latency"></div>',
+					'<div class="value">{HOST_LATENCY_MIN} / {HOST_LATENCY_AVG} / {HOST_LATENCY_MAX}</div>',
+				'</div>',
+			
+			'</div>',
+			
+			'<div class="icinga-monitor-performance-container-50">',
+			
+				'<div class="clearfix icinga-monitor-performance-container">',
+					'<div title="' + _('Services') + '" class="key icinga-icon-service"></div>',
+					'<div class="value">{NUM_ACTIVE_SERVICE_CHECKS} / {NUM_PASSIVE_SERVICE_CHECKS}</div>',
+				'</div>',
+				
+				'<div class="clearfix icinga-monitor-performance-container">',
+					'<div title="' + _('Service execution') + '" class="key icinga-icon-execution-time"></div>',
+					'<div class="value">{SERVICE_EXECUTION_TIME_MIN} / {SERVICE_EXECUTION_TIME_AVG} / {SERVICE_EXECUTION_TIME_MAX}</div>',
+				'</div>',
+				
+				'<div class="clearfix icinga-monitor-performance-container">',
+					'<div title="' + _('Service latency') + '" class="key icinga-icon-latency"></div>',
+					'<div class="value">{SERVICE_LATENCY_MIN} / {SERVICE_LATENCY_AVG} / {SERVICE_LATENCY_MAX}</div>',
+				'</div>',
+				
+			'</div>',
+			
+			'</div>',
+			
+			'</tpl>'
+		)
+	});
+	
+	CE.add(monitorPerformancePanel);
+	CE.doLayout();	
+});
+</script>
