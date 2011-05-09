fdca745f4e20af723a9668079ccf48b458ff918e
jmosshammer <jannis.mosshammer@netways.de>
2011-05-09 11:26:13 +0200

    * Removed id from update routine

--- etc/schema/updates/oracle_v1-3_to_1-4.sql
+++ etc/schema/updates/oracle_v1-3_to_1-4.sql
@@ -1,1 +1,1 @@
-INSERT INTO nsm_target (target_id,target_name,target_description,target_class,target_type) VALUES ('16','icinga.cronk.log','Enables icinga-log cronk','','credential');
+INSERT INTO nsm_target (target_name,target_description,target_class,target_type) VALUES ('icinga.cronk.log','Enables icinga-log cronk','','credential');
