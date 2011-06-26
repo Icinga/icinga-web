a216431bb4af0d8b1c927d1c73f32747e42ab579
Marius Hein <marius.hein@netways.de>
2011-06-26 12:06:27 +0200

    * Changed clear cache to agavi way

--- app/modules/AppKit/models/Tasks/ClearCacheModel.class.php
+++ app/modules/AppKit/models/Tasks/ClearCacheModel.class.php
@@ -3,50 +3,11 @@
 class AppKit_Tasks_ClearCacheModel extends AppKitBaseModel {
 
     public function clearCache() {
-        $cache_dir = AgaviConfig::get('core.cache_dir');
-
-        $content_dir = $cache_dir. '/content';
-
-        if (is_dir($content_dir)) {
-            $config_count = $this->removeFilesRecursive($content_dir);
-            $this->getContext()->getLoggerManager()->log(sprintf('ClearCache: Deleted %d cache (content) objects', $config_count), AgaviLogger::INFO);
-        }
-
-        $config_files = glob($cache_dir. '/config/*.php', GLOB_NOSORT);
-        $config_count = count($config_files);
-
-        if ($config_count > 0) {
-            foreach($config_files as $config_file) {
-                unlink($config_file);
-            }
-            $this->getContext()->getLoggerManager()->log(sprintf('ClearCache: Deleted %d cache (config) files', $config_count), AgaviLogger::INFO);
-        }
+        AgaviToolkit::clearCache();
+        $this->log('Cache cleared through webinterface', AgaviLogger::INFO);
+        return true;
     }
-
-    private function removeFilesRecursive($path) {
-        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
-
-        $order = array();
-
-        foreach($iterator as $fileObject) {
-            $order[] =$fileObject->getRealPath();
-        }
-
-        $count = count($order);
-
-        while (($file = array_pop($order))) {
-            if (is_dir($file)) {
-                rmdir($file);
-            }
-
-            elseif(is_file($file)) {
-                unlink($file);
-            }
-        }
-
-        return $count;
-    }
-
+    
 }
 
 ?>
