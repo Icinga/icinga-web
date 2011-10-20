43470e67e652124077378cb098fa50986a56f3a7
Jannis Moßhammer <jannis.mosshammer@netways.de>
2011-10-20 17:44:39 +0200

    * Updated objectgrid (Working state, refs #1941)

--- app/modules/Cronks/lib/js/Cronks/Tackle/Icinga.Cronks.Tackle.ObjectGrid.js
+++ app/modules/Cronks/lib/js/Cronks/Tackle/Icinga.Cronks.Tackle.ObjectGrid.js
@@ -2,7 +2,16 @@ Ext.ns('Icinga.Cronks.Tackle');
 
 Icinga.Cronks.Tackle.ObjectGrid = Ext.extend(Ext.grid.GridPanel, {
 	title : 'Object tree',
-	
+    downTimeQTip : _('Host is currently in a downtime'),
+    acknowledgedQTip : _('Host problem has been acknowledged'),
+    viewConfig: {
+   //     forceFit: true,
+        getRowClass: function(record,index) {
+
+            if(parseInt(record.get('HOST_SCHEDULED_DOWNTIME_DEPTH'),10) > 0)
+                return 'icinga-row-downtime ';
+        }
+    },
 	constructor : function(config) {
 		
 		config = Ext.apply(config || {}, {
@@ -14,31 +23,58 @@ Icinga.Cronks.Tackle.ObjectGrid = Ext.extend(Ext.grid.GridPanel, {
 	
 	initComponent : function() {
 		
-		this.store = new Ext.data.ArrayStore({
-			autoDestroy : true,
-			idIndex : 0,
-			fields : ['object_id', 'object_name'],
-			data : [
-			 [1, 'test-host-1'],
-			 [2, 'test-host-2'],
-			 [3, 'test-host-3'],
-			 [4, 'test-host-4']
-			]
-		});
-		
+		this.store = new Icinga.Api.RESTStore({
+            target: 'host',
+            columns: [
+                'HOST_ID',
+                'HOST_NAME',
+                'HOST_CURRENT_STATE',
+                'HOST_OBJECT_ID',
+                'HOST_LAST_CHECK',
+                'HOST_SCHEDULED_DOWNTIME_DEPTH',
+                'HOST_PROBLEM_HAS_BEEN_ACKNOWLEDGED'
+            ]
+        
+        })
+		this.store.load();
 		this.cm = new Ext.grid.ColumnModel({
-			defaults : {
-				width : 200,
-				
-			},
+
 			columns : [{
-				id : 'object_id',
-				header : _('OID'),
-				dataIndex : 'object_id'
-			}, {
-				header : _('Name'),
-				dataIndex : 'object_name'
-			}]
+				header : _('State'),
+				dataIndex : 'HOST_CURRENT_STATE',
+                columnWidth: 25,
+                width; 25,
+                renderer: function(value, metaData, record, rowIndex, colIndex, store) {
+                    value = parseInt(value,10);
+                    switch(value) {
+                        case 0:
+                            metaData.css = 'icinga-status-up';
+                            break;
+                        case 1:
+                            metaData.css = 'icinga-status-down';
+                            break;
+                        case 2:
+                            metaData.css = 'icinga-status-unreachable';
+                            break;
+                        case 99:
+                            metaData.css = 'icinga-status-pending';
+                            break;
+                    }
+                    if(parseInt(record.get('HOST_SCHEDULED_DOWNTIME_DEPTH'),10) > 0) {
+                        return "<div class='icinga-icon-info-downtime' style='width:25px;height:16px;margin:auto' qtip='"+this.downTimeQTip+"'></div>";
+                    } 
+                    if(value > 0 && parseInt(record.get('HOST_PROBLEM_HAS_BEEN_ACKNOWLEDGED'),10) === 1)
+                        return "<div class='icinga-icon-info-problem-acknowledged' style='width:25px;height:16px;margin:auto' qtip='"+this.acknowledgedQTip+"'></div>";
+                    return "";
+                },
+                scope:this
+            },{
+                header: _('Host'),
+                dataIndex : 'HOST_NAME'
+            },{
+                header: _('Last check'),
+                dataIndex : 'HOST_LAST_CHECK'
+            }]
 		});
 		
 		Icinga.Cronks.Tackle.ObjectGrid.superclass.initComponent.call(this);
