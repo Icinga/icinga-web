f8a0598d065cc175382cc5d94ecba81cfd42f554
Marius Hein <marius.hein@netways.de>
2012-01-31 13:51:25 +0100

    * Json unauthorized error fix

--- app/modules/AppKit/views/SecureSuccessView.class.php
+++ app/modules/AppKit/views/SecureSuccessView.class.php
@@ -1,12 +1,27 @@
 <?php
 
 class AppKit_SecureSuccessView extends AppKitBaseView {
-    public function executeHtml(AgaviRequestDataHolder $rd) {
-        $this->setAttribute('_title', 'Access Denied');
 
-        $this->setupHtml($rd);
+    public function initialize(AgaviExecutionContainer $container) {
+        parent::initialize($container);
+    }
 
+    private function sendHeader() {
         $this->getResponse()->setHttpStatusCode('401');
+    }
+
+    public function executeHtml(AgaviRequestDataHolder $rd) {
+        $this->sendHeader();
+        $this->setAttribute('_title', 'Access Denied');
+        $this->setupHtml($rd);
+    }
+
+
+    public function executeJson(AgaviRequestDataHolder $rd) {
+        return json_encode(array(
+            'success' => false,
+            'errors' => array('401 unauthorized')
+        ));
     }
 }
 
