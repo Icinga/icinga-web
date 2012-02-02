dfc26662f4db90be960cce1c66195a137c720cf4
Marius Hein <marius.hein@netways.de>
2012-02-02 13:34:17 +0100

    * Fix for working with models and different databases

--- app/modules/Cronks/lib/model/CronksBaseModel.class.php
+++ app/modules/Cronks/lib/model/CronksBaseModel.class.php
@@ -4,7 +4,10 @@
  * The base model from which all Cronks module models inherit.
  */
 class CronksBaseModel extends AppKitBaseModel {
-    // EMPTY
+    public function initialize(AgaviContext $context, array $parameters = array()) {
+        parent::initialize($context, $parameters);
+        IcingaDoctrineDatabase::resetCurrentConnection();
+    }
 }
 
 ?>
