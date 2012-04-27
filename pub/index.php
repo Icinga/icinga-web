549c05209a3641050d81e8ddbb9b0d9bd794a85c
Jannis Mosshammer <jannis.mosshammer@netways.de>
2012-04-27 11:52:55 +0200

    Added xls extension check before initializing agavi fixes #2564

--- pub/index.php
+++ pub/index.php
@@ -9,6 +9,11 @@ header("Access-Control-Max-Age: 720000");
 // just make sure a default timezone exists
 if(!ini_get("date.timezone"))
 	ini_set("date.timezone","GMT");
+
+// check for xsl BEFORE loading agavi, otherwise an obsucure error will be thrown
+if(!extension_loaded('xsl')) {
+    die("Please install the php xsl extension, as this is required for icinga-web");    
+}
 // +---------------------------------------------------------------------------+
 // | An absolute filesystem path to the agavi/agavi.php script.                |
 // +---------------------------------------------------------------------------+
