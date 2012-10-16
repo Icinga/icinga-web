098fe339352e3a8c48200d32a13dd1db19f4959a
Marius Hein <marius.hein@netways.de>
2012-10-16 16:04:53 +0200

    Fixes small reporting issues
    Bug if the loading mask is destroyed, better initial message

--- app/modules/Reporting/lib/js/Icinga.Reporting.util.RunReportPanel.js
+++ app/modules/Reporting/lib/js/Icinga.Reporting.util.RunReportPanel.js
@@ -73,10 +73,13 @@ Icinga.Reporting.util.RunReportPanel = Ext.extend(Icinga.Reporting.abstract.Appl
         Icinga.Reporting.util.RunReportPanel.superclass.initComponent.call(this);
         
         this.add({
-            xtype : 'panel', 
-            html : '<div style="padding: 20px;"><h3>'
-                + _('... please select a report from the left tree view')
-                + '</h3></div>'
+            layout : 'fit',
+            html : String.format('<h1>{0}</h1>{1}',
+                _('No report selected'), 
+                _('Please select a report from the tree panel on the left')
+            ),
+            border : false,
+            cls : 'simple-content-box'
         });
         
         this.setToolbarEnabled(false);
@@ -181,7 +184,10 @@ Icinga.Reporting.util.RunReportPanel = Ext.extend(Icinga.Reporting.abstract.Appl
     parseOutput : function(response, options) {
         this.parameterData = Ext.util.JSON.decode(response.responseText);
         
-        this.loadingMask.hide();
+        if (!Ext.isEmpty(this.loadingMask)) {
+            this.loadingMask.hide();
+        }
+        
         delete this.loadingMask;
         
         this.buildInterface(this.parameterData);
