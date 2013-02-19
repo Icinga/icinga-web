ba0e886f8f84a4e287c0c383703cd4a8b0e395bc
Markus Frosch <markus@lazyfrosch.de>
2013-02-19 15:07:17 +0100

    Updated CustomVariable Credential
    * multiple CVs aggregated by OR
    * LIKE wildcards

--- app/modules/Api/models/Views/Extender/CustomVariableExtenderModel.class.php
+++ app/modules/Api/models/Views/Extender/CustomVariableExtenderModel.class.php
@@ -66,11 +66,11 @@ class Api_Views_Extender_CustomVariableExtenderModel extends IcingaBaseModel
             "cv_name" => "varname",
             "cv_value" => "varvalue"
         );
+        $pairs = array();
         foreach($targetVals as $cvKeyValuePair) {
-            $query->andWhere(
-                "($aliasAbbr.".$keymap[$cvKeyValuePair["tv_key"]]." = '".$cvKeyValuePair["tv_val"]."'
-                    $objectTypeClause  ".$whereAppendix.")"
-            );
+            $pairs[] = "($aliasAbbr.".$keymap[$cvKeyValuePair["tv_key"]]." LIKE '".$cvKeyValuePair["tv_val"]."'
+                        $objectTypeClause  ".$whereAppendix.")";
         }
+        $query->andWhere(join(" OR ", $pairs));
     }
 }
