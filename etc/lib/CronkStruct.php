7a9b9367384bdb2cf0f29ad514c82cf470557467
Marius Hein <marius.hein@netways.de>
2013-04-03 15:56:25 +0200

    Icinga-web update 1.8 - Custom cronks don't work anymore
    Add phing task to run upgrade on the cronks format to
    add missing "event-sub-frame" column.
    
    fixes #3431

---
+++ etc/lib/CronkStruct.php
@@ -0,0 +1,303 @@
+<?php
+// {{{ICINGA_LICENSE_CODE}}}
+// -----------------------------------------------------------------------------
+// This file is part of icinga-web.
+//
+// Copyright (c) 2009-2013 Icinga Developer Team.
+// All rights reserved.
+//
+// icinga-web is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// icinga-web is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with icinga-web.  If not, see <http://www.gnu.org/licenses/>.
+// -----------------------------------------------------------------------------
+// {{{ICINGA_LICENSE_CODE}}}
+
+class CronkStruct {
+
+    /**
+     * Cronk record to modify
+     * @var Cronk
+     */
+    private $cronk;
+
+    /**
+     * Dom of xml struct
+     * @var DOMDocument
+     */
+    private $dom;
+
+    /**
+     * Json state data
+     * @var stdClass
+     */
+    private $json;
+
+    /**
+     * Cronk UID
+     * @var string
+     */
+    private $uid;
+
+    /**
+     * Cronk DB UID
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * Human readable name of cronk
+     * @var string
+     */
+    private $name;
+
+    public function __construct(Cronk $record=null)
+    {
+        $this->dom = new DOMDocument('1.0', 'utf-8');
+        $this->xpath = new DOMXPath($this->dom);
+        $this->xpath->registerNamespace('ae', 'http://agavi.org/agavi/config/global/envelope/1.0');
+
+        if ($record !== null) {
+            $this->setCronkRecord($record);
+        }
+    }
+
+    public function setCronkRecord(Cronk $record)
+    {
+        $this->setId($record->cronk_id);
+        $this->setUid($record->cronk_uid);
+        $this->setName($record->cronk_name);
+
+        if ($record->cronk_xml) {
+            $this->setXml($record->cronk_xml);
+        }
+
+        $this->cronk = $record;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param \stdClass $json
+     */
+    public function setJson($json)
+    {
+        $this->json = $json;
+    }
+
+    /**
+     * @return \stdClass
+     */
+    public function getJson()
+    {
+        return $this->json;
+    }
+
+    /**
+     * @param string $uid
+     */
+    public function setUid($uid)
+    {
+        $this->uid = $uid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets xml and initialize depending objects
+     * @param string $xml xml dump
+     */
+    public function setXml($xml)
+    {
+        $this->dom->loadXML($xml);
+        $this->getXPathInstance(true);
+
+        $state = $this->getXmlParam('state');
+        if ($state) {
+            $this->setJson(json_decode($state, true));
+        }
+    }
+
+    /**
+     * Manages Xpath instance on this dom
+     * @param bool $fresh
+     * @return DOMXPath
+     */
+    public function getXPathInstance($fresh=false)
+    {
+        static $xpath = null;
+
+        if ($xpath === null || $fresh === true) {
+            $xpath = null;
+            $xpath = new DOMXPath($this->dom);
+            $xpath->registerNamespace('ae', 'http://agavi.org/agavi/config/global/envelope/1.0');
+        }
+
+        return $xpath;
+    }
+
+    /**
+     * Returns parameter value from xml
+     * @param $name Parameter name
+     * @return null|string
+     */
+    public function getXmlParam($name)
+    {
+
+        $xpath = $this->getXPathInstance();
+        $query = '/cronk/ae:parameter/ae:parameter[@name=\'' . $name. '\']';
+        $nodes = $xpath->query($query);
+
+        if ($nodes->length>0) {
+            $node = $nodes->item(0);
+            return $node->textContent;
+        }
+
+        return null;
+    }
+
+    public function writeXmlParam($name, $content, $cdata=true)
+    {
+        $xpath = $this->getXPathInstance();
+        $query = '/cronk/ae:parameter/ae:parameter[@name=\'' . $name. '\']';
+        $nodes = $xpath->query($query);
+
+        if ($nodes->length>0) {
+            $node = $nodes->item(0);
+            while ($node->hasChildNodes()) {
+                $child = $node->firstChild;
+                $node->removeChild($child);
+            }
+
+            $sub = null;
+
+            if ($cdata) {
+                $sub = $this->dom->createCDATASection($content);
+            } {
+                $sub = $this->dom->createTextNode($content);
+            }
+
+            $node->appendChild($sub);
+        }
+    }
+
+    public function persistToDatabase()
+    {
+        $this->writeXmlParam('state', json_encode($this->getJson()), true);
+        $this->cronk->cronk_xml = $this->dom->saveXML();
+        $return = ($this->cronk->state() === Doctrine_Record::STATE_CLEAN) ? false : true;
+        $this->cronk->save();
+        return $return;
+    }
+
+    // ------------------------------------------------------------------------
+    // Modifying methods
+    // ------------------------------------------------------------------------
+
+    /**
+     * Upgrade cronk event sub frame
+     *
+     * This is needed to use cronk below 1.8 with 1.8 event expander row
+     *
+     * @return bool
+     */
+    public function upgradeEventSubFrame()
+    {
+        $module = $this->getXmlParam('module');
+        $action = $this->getXmlParam('action');
+        $return = true;
+
+        if ($module !== 'Cronks' || $action !== 'System.ViewProc' || !is_array($this->getJson())) {
+            return false;
+        }
+
+        $columns = $this->json['nativeState']['columns'];
+
+        if (!isset($columns[1]) || $columns[1]['id'] !== 'event-sub-frame' || $columns[1]['width'] !== 20) {
+            $newCol = array(
+                'id'        => 'event-sub-frame',
+                'width'     => 20
+            );
+
+            array_splice($columns, 1, 0, array($newCol));
+
+            $this->json['nativeState']['columns'] = $columns;
+            $return = true;
+        }
+
+        if (isset($this->json['colModel']['columns'])) {
+            $columns = $this->json['colModel']['columns'];
+            $found = 0;
+
+            foreach ($columns as $index=>$column) {
+                if (isset($column['id']) && $column['id'] === 'event-sub-frame') {
+                    $found = $index;
+                    break;
+                }
+            }
+
+            $newCol = array(
+                'width'         => 20,
+                'dataIndex'     => 'id',
+                'id'            => 'event-sub-frame',
+                'sortable'      => false
+            );
+
+
+            if ($found>0) {
+                $columns[$found] = $newCol;
+            } else {
+                $columns[] = $newCol;
+                $return = true;
+            }
+
+            $this->json['colModel']['columns'] = $columns;
+        }
+
+        return $return;
+    }
+}
---
+++ etc/lib/cronkUpgradeTask.php
@@ -0,0 +1,62 @@
+<?php
+// {{{ICINGA_LICENSE_CODE}}}
+// -----------------------------------------------------------------------------
+// This file is part of icinga-web.
+// 
+// Copyright (c) 2009-2013 Icinga Developer Team.
+// All rights reserved.
+// 
+// icinga-web is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// icinga-web is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with icinga-web.  If not, see <http://www.gnu.org/licenses/>.
+// -----------------------------------------------------------------------------
+// {{{ICINGA_LICENSE_CODE}}}
+
+require 'agaviConsoleTask.php';
+require 'CronkStruct.php';
+
+class cronkUpgradeTask extends agaviConsoleTask {
+
+    public function main() {
+        parent::main();
+
+       $cronksQuery = Doctrine_Query::create()
+       ->select('*')
+       ->from('Cronk c');
+
+        $cronks = $cronksQuery->execute();
+
+        /** @var $cronk Cronk */
+        $cronk = null;
+
+        /** @var $structs CronkStruct[] */
+        $structs = array();
+
+        foreach ($cronks as $cronk) {
+            $cronkStruct = new CronkStruct($cronk);
+            $this->log('Testing cronk '. $cronkStruct->getName());
+            $status = $cronkStruct->upgradeEventSubFrame();
+            if ($status === true) {
+                $update = $cronkStruct->persistToDatabase();
+                if ($update) {
+                    $this->log('Columns upgraded', Project::MSG_WARN);
+                    continue;
+                }
+            }
+
+            $this->log('Nothing to to', Project::MSG_INFO);
+
+        }
+    }
+
+
+}
