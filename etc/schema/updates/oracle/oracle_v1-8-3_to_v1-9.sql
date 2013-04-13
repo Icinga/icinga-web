925b48aaaa90d8d16b0d102f50bf8140f78a52b3
Thomas Dressler <tdressler@tdressler.net>
2013-04-13 20:42:57 +0200

    Oracle: add missed commit and error handler to upgrade v1.8.3 -to- v1.9.0

--- etc/schema/updates/oracle/oracle_v1-8-3_to_v1-9.sql
+++ etc/schema/updates/oracle/oracle_v1-8-3_to_v1-9.sql
@@ -1,3 +1,6 @@
+--error handler
+whenever sqlerror exit failure
+
 -- Default version bump
 DELETE FROM nsm_db_version;
 INSERT INTO nsm_db_version VALUES ('1','icinga-web/v1.9.0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
@@ -10,3 +13,7 @@ ALTER TABLE nsm_user
 ALTER TABLE
   nsm_target
   add constraint target_key_unique_target_name_uq UNIQUE (target_name);
+
+--final commit 
+commit;
+
