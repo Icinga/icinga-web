da10d25fc0feebc71bf76feffdb4b9eebd8fa9bd
Michael Friedrich <Michael.Friedrich@netways.de>
2014-01-09 00:24:35 +0100

    Update copyright to present.
    Refs #5423

--- app/modules/Cronks/lib/js/Cronk/grid/renderer/ColumnRenderer.js
+++ app/modules/Cronks/lib/js/Cronk/grid/renderer/ColumnRenderer.js
@@ -2,7 +2,7 @@
 // -----------------------------------------------------------------------------
 // This file is part of icinga-web.
 // 
-// Copyright (c) 2009-2013 Icinga Developer Team.
+// Copyright (c) 2009-present Icinga Developer Team.
 // All rights reserved.
 // 
 // icinga-web is free software: you can redistribute it and/or modify
