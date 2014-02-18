fd76752fa6cf8d37f445d85bd1fb96a9ac4691ce
Markus Frosch <markus@lazyfrosch.de>
2014-02-18 13:38:20 +0100

    Removed unnecessary try/catch block on ApiCommand.
    Adopted from GitHub pull request ##1
    
    Fixes #5465
    Refs #5013

--- app/modules/Api/actions/ApiCommandAction.class.php
+++ app/modules/Api/actions/ApiCommandAction.class.php
@@ -36,15 +36,11 @@ class Api_ApiCommandAction extends IcingaApiBaseAction {
         if (!$this->context->getUser()->isAuthenticated() || !$this->context->getUser()->hasCredential('icinga.user')) {
             return array('Api', 'GenericError');
         }
-        try {
-            if($this->context->getUser()->getNsmUser()->getTarget('IcingaCommandRo')) {
-                $errors = array('Commands are disabled for this user');
-                $this->getContainer()->setAttributeByRef('errors', $errors, 'org.icinga.api.auth');
-                $this->getContainer()->setAttribute('success', false, 'org.icinga.api.auth');
-                return array('Api', 'GenericError');
-            }
-        } catch (AppKitDoctrineException $e) {
-            // PASS
+        if ($this->context->getUser()->getNsmUser()->getTarget('IcingaCommandRo')) {
+            $errors = array('Commands are disabled for this user');
+            $this->getContainer()->setAttributeByRef('errors', $errors, 'org.icinga.api.auth');
+            $this->getContainer()->setAttribute('success', false, 'org.icinga.api.auth');
+            return array('Api', 'GenericError');
         }
         $command = $rd->getParameter("command");
         
